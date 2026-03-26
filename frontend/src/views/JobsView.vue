f86bf758954d98004e309e5e96d4ace4bbb1518a
Frank Refol <t3knoid@users.noreply.github.com>
2026-03-26 15:22:28 -0400

    UI Phase 3: Theme System & Localization

--- frontend/src/views/JobsView.vue
+++ frontend/src/views/JobsView.vue
@@ -1,6 +1,11 @@
+<script setup>
+import { useI18n } from 'vue-i18n'
+const { t } = useI18n()
+</script>
+
 <template>
   <div>
-    <h1>Jobs</h1>
-    <p>Jobs view will be implemented in a future phase.</p>
+    <h1>{{ t('jobs.title') }}</h1>
+    <p>{{ t('jobs.placeholder') }}</p>
   </div>
 </template>
