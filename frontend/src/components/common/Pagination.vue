f4322aa64020898cde4ed6d390212cf76d68d607
Frank Refol <t3knoid@users.noreply.github.com>
2026-03-30 16:42:18 -0400

    feat: update pagination range display to use localized labels

--- frontend/src/components/common/Pagination.vue
+++ frontend/src/components/common/Pagination.vue
@@ -41,7 +41,7 @@ function goToPage(nextPage) {
 
 <template>
   <div class="pagination-wrap" role="navigation" :aria-label="t('common.labels.pagination')">
-    <span class="pagination-range">{{ startIndex }}-{{ endIndex }} / {{ total }}</span>
+    <span class="pagination-range">{{ t('common.labels.range', { start: startIndex, end: endIndex, total }) }}</span>
     <button type="button" class="btn page-btn" :disabled="page <= 1" @click="goToPage(page - 1)">{{ t('common.actions.previous') }}</button>
     <span class="page-label">{{ page }} / {{ totalPages }}</span>
     <button type="button" class="btn page-btn" :disabled="page >= totalPages" @click="goToPage(page + 1)">{{ t('common.actions.next') }}</button>
